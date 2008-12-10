528591abba037daaedbc30d6515322f892af491a
Florian Sprenger <f.sprenger@4teamwork.ch>
2008-12-10 14:24:27 +0000

    finished content type definition, unfortunately without date / time field (yet)

--- izug/poodle/content/poodle.py
+++ izug/poodle/content/poodle.py
@@ -5,28 +5,85 @@ try:
 except ImportError:
     from Products.Archetypes import atapi
 
+from AccessControl import ClassSecurityInfo
 from Products.ATContentTypes.content import base
 
 from Products.ATContentTypes.content import schemata
 from Products.CMFCore import permissions
+from Products.CMFCore.utils import getToolByName
+
+from Products.AddRemoveWidget import AddRemoveWidget
+from Products.DataGridField import DataGridField, DataGridWidget
+from Products.DataGridField.Column import Column
 
 
+from izug.poodle.datagridextension.CalendarColumn import CalendarColumn
 from izug.poodle import poodleMessageFactory as _
 from izug.poodle.interfaces import IPoodle
 from izug.poodle.config import PROJECTNAME
 
-
 PoodleSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
+    atapi.LinesField(
+        name='users',
+        vocabulary="getPossibleUsers",
+        widget=AddRemoveWidget
+        (
+            label="Users",
+            label_msgid='izugpoodle_label_users',
+            i18n_domain='izugpoodle',
+        ),
+        required=1,
+        multivalued=1
+    ),
+
+    DataGridField(
+        name='dates',
+        widget=DataGridWidget(
+            columns= {"date": Column("Date (TT. MM. JJJJ)"), "duration": Column("Duration")},
+            label='Dates',
+            label_msgid='izugpoodle_label_users',
+            i18n_domain='izugpoodle',
+        ),
+        columns= ("date", "duration")
+    ),
+    
+    # XXX: Ein Datumsfeld waere schoen; wird von Datagridfield noch nicht unterstuetzt, deshalb habe ich selber mit "CalendarColumn" angefangen.
+    # Funktioniert leider noch nicht. So wuerde es gehen:
+    #
+    # DataGridField(
+    #     name='dates',
+    #     widget=DataGridWidget(
+    #         columns= {"date": CalendarColumn("Date"), },
+    #         label='Dates',
+    #         label_msgid='izugpoodle_label_users',
+    #         i18n_domain='izugpoodle',
+    #     ),
+    #     columns= ("date",)
+    # ),
 ))
 
 schemata.finalizeATCTSchema(PoodleSchema, moveDiscussion=False)
 
 
 class Poodle(base.ATCTContent):
-    """ A 'doodle'-like content type that helps in finding a date for a meeting """
+    """ A 'doodle'-like content type that helps finding a date for a meeting """
     implements(IPoodle)
-
+    security = ClassSecurityInfo()
+    
     portal_type = "Meeting poll"
     schema = PoodleSchema
+    
+    security.declarePrivate("getPossibleUsers")
+    def getPossibleUsers(self):
+        pas, mtool = getToolByName(self, "acl_users"), getToolByName(self, "portal_membership") 
+        result = []
+        userids = [u['userid'] for u in pas.searchUsers()]
+        userids.sort()
+        
+        for userid in userids:
+            user = mtool.getMemberById(userid) 
+            result.append((userid, user.getProperty('fullname')))
+        return result
+        
     
 atapi.registerType(Poodle, PROJECTNAME)
---
+++ izug/poodle/datagridextension/CalendarColumn.py
@@ -0,0 +1,58 @@
+from __future__ import nested_scopes
+
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass
+from Products.DataGridField.Column import Column
+
+class CalendarColumn(Column):
+    """ Column with calendar widget
+    """
+    security = ClassSecurityInfo()
+        
+    security.declarePublic('getMacro')
+    def getMacro(self):
+        """ Return macro used to render this column in view/edit """
+        return "datagrid_calendar_cell"
+        
+    security.declarePublic('processCellData')
+    def processCellData(self, form, value, context, field, columnId):
+        """ Read cell values from raw form data
+        
+        Column processing in forms may need special preparations for data if
+        widgets use other than <input value> for storing their
+        values in fields.
+        
+        @param form Submitted form, contains HTML fields
+        @param context Archetypes item instance for the submitted form
+        @param field Assigned field for this widget
+        @param columnId Column what we are operating
+        
+        @return new values which are constructed by processing data
+        """
+        import pdb; pdb.set_trace()
+        newValue = []
+        
+        for row in value:
+            
+            # we must clone row since
+            # row is readonly ZPublished.HTTPRequest.record object
+            newRow = {}
+            for key in row.keys():
+                newRow[key] = row[key]
+                        
+            orderIndex = row["orderindex_"] 
+            cellId = "%s_%s_%s" % (field.getName(), columnId, orderIndex)
+            if form.has_key(cellId):
+                # If check button is set in HTML form
+                # it's id appears in form of field.column.orderIndex
+                # field value is hardcoded to '1'
+                newRow[columnId] = form[cellId]
+            else:
+                # if item is not in form, user did not check it.
+                newRow[columnId] = ''
+                
+            newValue.append(newRow)
+            
+        return newValue
+
+InitializeClass(CalendarColumn)
