aea46455260f8467b75c4ce38ec1c3f2b80254ac
Florian Sprenger <f.sprenger@4teamwork.ch>
2008-12-16 12:56:58 +0000

    mail notification, fixed an error with events (event was not fired when object was created)

--- izug/poodle/browser/views.py
+++ izug/poodle/browser/views.py
@@ -7,7 +7,7 @@ from zope.component import queryUtility
 
 from plone.i18n.normalizer.interfaces import IURLNormalizer
 
-
+from izug.poodle import poodleMessageFactory as _
 from izug.poodle.interfaces import IPoodle, IPoodleConfig
 
 
@@ -40,6 +40,32 @@ class PoodleView(BrowserView):
             if values == ['']:
                 return
             self.context.saveUserData(user.id, values)
+            self.sendNotification(user)
+            
+        
+    def sendNotification(self, user):
+        """Sends a notification after someone filled out the meeting poll"""
+        mtool = getToolByName(self.context, "portal_membership") 
+        portal = getToolByName(self.context, 'portal_url').getPortalObject()
+        site_properties = getToolByName(self.context, 'portal_properties').site_properties
+        
+        host = getToolByName(self.context, "MailHost")
+        creator = self.context.Creator() # send a mail to the creator of the poll
+        send_to_address = mtool.getMemberById(creator).getProperty('email')
+        if send_to_address == '': send_to_address = site_properties.email_from_address
+        send_from_address = site_properties.email_from_address
+        subject = u"%s %s" % (_(u"izugpoodle_mail_subject", default="Update on meeting poll at"), self.context.absolute_url())
+#        import pdb; pdb.set_trace()
+        template = getattr(self.context, 'poodle_notification')
+        encoding = portal.getProperty('email_charset')
+        envelope_from = send_from_address
+        # Cook from template
+        message = template(self,  username=self.getUserFullname(user.id), url=self.context.absolute_url())
+        result = host.secureSend(message, send_to_address,
+                                 envelope_from, subject=subject,
+                                 subtype='plain', charset=encoding,
+                                 debug=False, From=send_from_address)
+
             #{'form.button.Save': 'Speichern', 'hamu2.12.2008': '2.12.2008', 'hamu13.7.82': '13.7.82'}
         
         
