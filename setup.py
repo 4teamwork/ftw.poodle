d26ca58cfa9ca15ba5c98f459fdbde126f1de07c
Jonas Baumann <j.baumann@4teamwork.ch>
2009-07-06 16:03:50 +0000

    updated setup files, history, license

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.1'
+version = '0.1-dev'
+maintainer = 'Mathias Leimgruber'
 
 long_description = (
     read('README.txt')
@@ -36,7 +37,7 @@ tests_require=['zope.testing']
 
 setup(name='izug.poodle',
       version=version,
-      description="A product to make polls to find out when to have a meeting",
+      description="A product to make polls to find out when to have a meeting (Maintainer: %s)" % maintainer,
       long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -46,10 +47,10 @@ setup(name='izug.poodle',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
       keywords='',
-      author='4teamwork GmbH',
-      author_email='info@4teamwork.ch',
-      url='http://www.4teamwork.ch',
-      license='GPL',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/kunden/izug/izug.poodle/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['izug', ],
       include_package_data=True,
