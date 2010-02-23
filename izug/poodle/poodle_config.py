94023973ef76ba07607e6bf8200eea42aec0925a
Pascal Habegger <p.habegger@4teamwork.ch>
2010-02-23 16:38:07 +0000

    use persistendDict instead of regular not so persisten dict

--- izug/poodle/poodle_config.py
+++ izug/poodle/poodle_config.py
@@ -2,6 +2,8 @@ from zope.interface import implements
 from zope.component import adapts
 from zope.annotation.interfaces import IAnnotations
 from interfaces import IPoodle, IPoodleConfig
+from persistent.dict import PersistentDict 
+
 
 class PoodleConfig(object):
     implements(IPoodleConfig)
@@ -12,11 +14,11 @@ class PoodleConfig(object):
         self.annotations = IAnnotations(self.context)
     
     def getPoodleData(self):
-        return self.annotations.get('poodledata', {})
+        return self.annotations.get('poodledata', PersistentDict({}))
     
     def setPoodleData(self, data):
         if data:
-            self.annotations['poodledata'] = data
+            self.annotations['poodledata'] = PersistentDict(data)
             
 
     
