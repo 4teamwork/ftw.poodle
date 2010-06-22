c9de0dfe8f12afc4ab9892e9b8ba1f1665c482fd
Jonas Baumann <j.baumann@4teamwork.ch>
2010-06-22 09:35:01 +0000

    Fixed import error in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@
 """
 This module contains the tool of izug.poodle
 """
-import os
 from setuptools import setup, find_packages
 
 def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    return open('/'.join(rnames)).read()
 
 version = open('izug/poodle/version.txt').read().strip()
 maintainer = 'Mathias Leimgruber'
