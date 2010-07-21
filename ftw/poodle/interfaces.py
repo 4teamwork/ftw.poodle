0b9ff6d5194a198b1008aa57291f42574dbf6f38
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-07-21 14:48:39 +0000

    pep8ig

--- ftw/poodle/interfaces.py
+++ ftw/poodle/interfaces.py
@@ -3,16 +3,18 @@ from zope.viewlet.interfaces import IViewletManager
 
 
 class IPoodle(Interface):
-    """A 'doodle'-like content type that 
+    """A 'doodle'-like content type that
     helps finding a date for a meeting
 
     """
 
+
 class IPoodleVotes(Interface):
     """Adapter for annotation storage of poodledata """
-    
+
+
 class IPoodletableBottom(IViewletManager):
     """Free slot to display other informations or integration of other
-    packages. 
+    packages.
 
     """
