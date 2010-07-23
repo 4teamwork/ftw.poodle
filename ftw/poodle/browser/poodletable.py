c128f7321bc4bf6a6a3e1c2c6c314bd70b4438c1
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-07-23 14:11:00 +0000

    fix poodleresults / added possibility to return a dict instead of html / added viewlet manager to poodletable

--- ftw/poodle/browser/poodletable.py
+++ ftw/poodle/browser/poodletable.py
@@ -1,23 +1,24 @@
-from Products.Five.browser import BrowserView 
-from Products.CMFCore.utils import getToolByName  
-from zope.component import getMultiAdapter 
+from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+from zope.component import getMultiAdapter
 from zope.component import queryUtility
 from plone.i18n.normalizer.interfaces import IURLNormalizer
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 
 class PoodleTableView(BrowserView):
     """Renders the poodle table
     The table is seperated from the poodleview, so we can reload
     the table per ajax.
     """
-    
+
     template = ViewPageTemplateFile('templates/poodletable.pt')
-    
+
     def __call__(self):
         """
         """
-        rc = getToolByName(self.context,'reference_catalog')
-        uid = self.context.REQUEST.get('uid',None)
+        rc = getToolByName(self.context, 'reference_catalog')
+        uid = self.context.REQUEST.get('uid', None)
         if uid:
             context = rc.lookupObject(uid)
 
@@ -26,57 +27,70 @@ class PoodleTableView(BrowserView):
         return self.template(uid=context.UID())
 
     def isCurrentUser(self, userid):
-        """returns bool value if the given user 
+        """returns bool value if the given user
         is the current loggedin user
+
         """
-        portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+        portal_state = getMultiAdapter(
+            (self.context, self.request),
+            name=u'plone_portal_state')
         user = portal_state.member()
         return user.id == userid
 
     def getUserFullname(self, userid):
         """returns fullname of a given user
         """
-        mtool = getToolByName(self.context, "portal_membership") 
+        mtool = getToolByName(self.context, "portal_membership")
         return mtool.getMemberById(userid).getProperty('fullname')
 
     def getCssClass(self, data):
         """returns three diffrent css class-names
-        depending on the data param. 
+        depending on the data param.
+
         """
-        if data == None: return "not_voted"
-        elif data == True: return "positive"
-        elif data == False: return "negative"
+        if data == None:
+            return "not_voted"
+        elif data == True:
+            return "positive"
+        elif data == False:
+            return "negative"
 
     def getInputId(self, user, date):
-        """generates the unique and normalized id for the checkboxes
-        """
+        """generates the unique and normalized id for the checkboxes"""
+        
         date = str(date)
         return queryUtility(IURLNormalizer).normalize(user + date)
 
-    def poodleResults(self,data=False):
+    def poodleResults(self, data=None, print_html=True):
         """count all user votes per date
         will be displayed at the bottom of the table
+
+        This method is traversable (allowed_attributes)
+
         """
         context = self.context.aq_inner
         if not data:
             data = context.getPoodleData()
         ids = data['ids']
         users = data['users'].values()
-        counted = []
+        result = []
 
         for base_d in ids:
             counter = 0
             for d in users:
                 if d[base_d]:
                     counter += 1
-            counted.append(counter)
-                    
-        # heighest value in list
-        h_value = max(counted)
-        
+            result.append(counter)
+            
+        # TODO: store the calculation in poodle_votes adapter
+        if not print_html:
+            data['result']= result
+            return data
+        h_value = max(result)
+
         # render a small part html for the last table row
         html_data = ''
-        for v in counted:
+        for v in result:
             if v == h_value:
                 v = "<b>%s</b>" % v
             html_data += "<td>%s</td>" % v
