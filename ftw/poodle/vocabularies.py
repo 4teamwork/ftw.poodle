b0a61e313828c3ccc0df6795094066eecf955f32
Jonas Baumann <jone@jone.ch>
2010-09-03 09:05:18 +0200

    try to use assignable_users vocabulary

--- ftw/poodle/vocabularies.py
+++ ftw/poodle/vocabularies.py
@@ -8,16 +8,25 @@ class AvailableUsersVocabulary(object):
     """
     lists all available users
     """
-    
+
     implements(IVocabularyFactory)
-    
+
     def __call__(self, context):
         """this utility calls plone.principalsource.Users utility
         so we can overwrite this one if we want a diffrent source.
-        """ 
+        """
         if context is None:
             context = getSite()
-        factory = component.getUtility(schema.interfaces.IVocabularyFactory, name='plone.principalsource.Users', context=context)
+        factory = component.queryUtility(
+            schema.interfaces.IVocabularyFactory,
+            name='assignable_users',
+            context=context)
+        if factory is None:
+            factory = component.getUtility(
+                schema.interfaces.IVocabularyFactory,
+                name='plone.principalsource.Users',
+                context=context)
+
         items = factory(context)
         return items
 
