abd9259a77e1a88a74476be756e01a32be0eff64
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-08-23 12:05:16 +0200

    Fix post request (save poodle votes)

--- ftw/poodle/browser/scripts/poodle.js
+++ ftw/poodle/browser/scripts/poodle.js
@@ -4,7 +4,7 @@ function sendPoodleForm(){
             e.preventDefault();
             var infos = $("#"+this.id).closest('form#poodleForm').serializeArray();
             var base_href = $('base')[0].href;
-            $.post(base_href+'/$_submit_data', infos, function(callback){
+            $.post(base_href+'/jq_submit_data', infos, function(callback){
                 $.post(base_href+'/ftw_poodle_table', infos, function(data){
                     $('#poodltablewrapper_'+infos[infos.length -1].value).html(data);
                 });
